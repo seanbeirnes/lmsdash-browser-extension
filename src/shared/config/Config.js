2056102fe3ebc981f7d37bf3de93ea1613b2f638
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2026-03-29 21:07:47 -0400

    Remove remaining .js files

--- src/shared/config/Config.js
+++
@@ -1,5 +0,0 @@
-export default class Config {
-  static APP_VERSION = __app_version;
-  static APP_DESCRIPTION = __app_description;
-  static DEBUG_MODE = (process.env.NODE_ENV === 'development');
-}
