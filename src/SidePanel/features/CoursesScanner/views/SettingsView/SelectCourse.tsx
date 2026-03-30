817d01495e3146b8f9618191006e38ffe756c0b1
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2026-03-29 21:27:42 -0400

    fix: stop useEffect from endless render loop

--- src/SidePanel/features/CoursesScanner/views/SettingsView/SelectCourse.tsx
+++ src/SidePanel/features/CoursesScanner/views/SettingsView/SelectCourse.tsx
@@ -54,7 +54,7 @@ function SelectCourse({ courseId, setCourseIds }: SelectCourseProps) {
     } else {
       setCourseIds([]);
     }
-  }, [course, setCourseIds]);
+  }, [data, setCourseIds]);
 
   if (!courseId) {
     return (
